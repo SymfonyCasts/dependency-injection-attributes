080927dda3a36c57cf835a3614babb5f785dbb43
Kevin Bond <kevinbond@gmail.com>
2024-10-15 19:22:12 -0400

    Step 40: Lazy: add Lazy attribute to PowerButton

--- src/Remote/Button/PowerButton.php
+++ src/Remote/Button/PowerButton.php
@@ -3,8 +3,10 @@
 namespace App\Remote\Button;
 
 use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
+use Symfony\Component\DependencyInjection\Attribute\Lazy;
 
 #[AsTaggedItem('power', priority: 50)]
+#[Lazy]
 final class PowerButton implements ButtonInterface
 {
     public function press(): void
