ad5038d7d66efac2847e9885b82f4f1a84a57dca
Kevin Bond <kevinbond@gmail.com>
2024-10-15 19:22:09 -0400

    Step 19: Log: Add LoggerRemote

---
+++ src/Remote/LoggerRemote.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Remote;
+
+use Psr\Log\LoggerInterface;
+
+final class LoggerRemote
+{
+    public function __construct(
+        private LoggerInterface $logger,
+        private ButtonRemote $inner,
+    ) {
+    }
+
+    public function press(string $name): void
+    {
+        $this->logger->info('Pressing button {name}', [
+            'name' => $name
+        ]);
+
+        $this->inner->press($name);
+
+        $this->logger->info('Pressed button {name}', [
+            'name' => $name
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function buttons(): iterable
+    {
+        return $this->inner->buttons();
+    }
+}
