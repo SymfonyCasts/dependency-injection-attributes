4f3924cd995245a5aaf01cb96e4a01ee0a4dc6fe
Kevin Bond <kevinbond@gmail.com>
2024-10-24 12:37:59 -0400

    Step 40: Lazy: use ParentalControls in VolumeUpButton

--- src/Remote/Button/VolumeUpButton.php
+++ src/Remote/Button/VolumeUpButton.php
@@ -2,13 +2,23 @@
 
 namespace App\Remote\Button;
 
+use App\Remote\ParentalControls;
 use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
 
 #[AsTaggedItem('volume-up', priority: 20)]
 final class VolumeUpButton implements ButtonInterface
 {
+    public function __construct(
+        private ParentalControls $parentalControls,
+    ) {
+    }
+
     public function press(): void
     {
+        if (true) { // detect if volume is too high
+            $this->parentalControls->volumeToHigh();
+        }
+
         dump('Change the volume up');
     }
 }
