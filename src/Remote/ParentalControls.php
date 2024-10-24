0f81d904a0b9600d44742c9b896dec0a93b4b922
Kevin Bond <kevinbond@gmail.com>
2024-10-24 12:37:58 -0400

    Step 39: Lazy: add ParentalControls

---
+++ src/Remote/ParentalControls.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Remote;
+
+use Symfony\Component\Mailer\MailerInterface;
+
+final class ParentalControls
+{
+    public function __construct(
+        private MailerInterface $mailer,
+    ) {
+    }
+
+    public function volumeToHigh(): void
+    {
+        dump('send volume alert email');
+    }
+}
